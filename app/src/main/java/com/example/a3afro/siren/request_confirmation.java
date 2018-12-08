6a6538ad387285e11462f1b2173970cfafe3763e
Omar Ali <free_bird1719@live.com>
2018-12-08 23:54:05 +0200

    initial commit

---
+++ app/src/main/java/com/example/a3afro/siren/request_confirmation.java
@@ -0,0 +1,13 @@
+package com.example.a3afro.siren;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class request_confirmation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_confirmation);
+    }
+}
