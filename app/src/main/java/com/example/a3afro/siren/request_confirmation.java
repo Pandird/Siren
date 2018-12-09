b5f58a52d82dfb17fccabacd8846ad6741db1371
Omar Ali <free_bird1719@live.com>
2018-12-09 02:50:18 +0200

    account settings and toolbar

--- app/src/main/java/com/example/a3afro/siren/request_confirmation.java
+++ app/src/main/java/com/example/a3afro/siren/request_confirmation.java
@@ -2,6 +2,7 @@ package com.example.a3afro.siren;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class request_confirmation extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class request_confirmation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_confirmation);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
