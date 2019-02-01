ab313c468d50625eeccf9373e463ee48448ffb90
Omar Ali <free_bird1719@live.com>
2019-02-01 08:29:40 +0200

    strings and circle account image

--- app/src/main/java/com/example/a3afro/siren/account_settings.java
+++ app/src/main/java/com/example/a3afro/siren/account_settings.java
@@ -1,7 +1,9 @@
 package com.example.a3afro.siren;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class account_settings extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class account_settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
+        TextView change_picture = (TextView) findViewById(R.id.change_picture);
+        change_picture.setPaintFlags(change_picture.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
