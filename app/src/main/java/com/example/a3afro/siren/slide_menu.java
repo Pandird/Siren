d72f3ba2cfd1929a3aa7a60363b02b8c137287fd
Omar Ali <free_bird1719@live.com>
2019-02-13 05:24:37 +0200

    google button and sign-up

--- app/src/main/java/com/example/a3afro/siren/slide_menu.java
+++ app/src/main/java/com/example/a3afro/siren/slide_menu.java
@@ -23,14 +23,7 @@ public class slide_menu extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -82,15 +75,9 @@ public class slide_menu extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+          if (id == R.id.nav_manage) {}
 
         }
 
